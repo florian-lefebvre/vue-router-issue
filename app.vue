a83026d41b76f6b46fcf3744079de4cadf4da56f
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-03-16 16:32:59 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <NuxtPage />
+    <p>SCROLL ALL THE WAY DOWN TO SEE THE LINKS</p>
+    <p>
+      SCROLL TO THE BOTTOM AND RELOAD. YOU'LL SEE SCROLLING HAPPENS AFTER A
+      SHORT DELAY
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab commodi
+      minima facilis nam quasi architecto ipsam aliquam soluta repudiandae.
+      Tempore ipsa corporis earum ullam nihil. Voluptatibus ullam provident
+      autem tenetur. Consectetur, aspernatur, laudantium id minima officiis
+      optio tempora reiciendis nobis doloremque voluptatum numquam quas eius,
+      animi illo. Rem odit doloribus at mollitia porro ullam. Ex, voluptatem
+      fugiat voluptas itaque labore incidunt odit minima optio eius architecto
+      deserunt! Aliquid obcaecati illo expedita enim explicabo, nam esse
+      perferendis aspernatur omnis voluptatibus ullam ratione quidem at
+      veritatis beatae deleniti reprehenderit deserunt cupiditate consectetur
+      aperiam consequatur ducimus. Ratione facere dolorum ea sint ut, blanditiis
+      ipsum quibusdam eaque distinctio reiciendis molestias sunt amet dolorem
+      est in sed ipsam corporis impedit qui ipsa vero doloremque nulla? Commodi
+      necessitatibus rem praesentium libero quos? Odit voluptatum beatae unde
+      rem animi! Similique dolore inventore, cum quis odio saepe tempore aperiam
+      repellendus magnam necessitatibus? Voluptatibus nobis temporibus accusamus
+      iusto rerum excepturi distinctio eius voluptate eaque, corporis, veritatis
+      repudiandae quae cum, magnam iste fuga natus? Excepturi eveniet odit
+      dolorum, officia alias aliquam expedita saepe itaque, nesciunt nobis id
+      officiis, qui veniam eos reprehenderit! Facilis animi quos odit eaque
+      iste, beatae porro eum alias dignissimos cum aliquam, neque sed tenetur
+      illo enim earum iure itaque voluptatem voluptate? Eveniet eius maiores
+      sed. Consectetur dolore facere ea labore eius ad accusamus provident
+      aliquam laudantium! Dolorem, incidunt accusantium labore tempora nihil
+      dignissimos, aliquid ipsa mollitia natus eaque praesentium laudantium!
+      Iusto magni dolorum ducimus molestiae, eligendi expedita illo impedit in
+      modi libero reprehenderit unde, praesentium qui quisquam dolorem natus
+      inventore? Provident accusamus, itaque, quia a unde necessitatibus
+      reiciendis sed accusantium, nihil ducimus quae. Illo, sunt ad saepe neque,
+      unde explicabo blanditiis nisi ratione error harum deleniti veniam
+      quibusdam hic a laboriosam sequi esse, quod tempore. Rem alias quo nisi
+      sit! Quidem necessitatibus tenetur in vitae, ipsam consectetur veritatis
+      magni numquam nesciunt alias praesentium natus perferendis aliquam commodi
+      ex nam deleniti. Cum nostrum sed voluptas ex modi sint perspiciatis
+      debitis minima nemo autem veniam nihil odio, aliquam est! Iusto velit
+      beatae pariatur eligendi recusandae officia cumque similique illo,
+      deleniti tenetur at nisi veniam voluptates repudiandae! Inventore ex
+      tempore aspernatur ipsum delectus magni perferendis eligendi odit eveniet,
+      amet nemo porro voluptatem ratione odio necessitatibus aliquid in
+      assumenda exercitationem dolores impedit, illo fugiat? Similique, illo
+      eligendi rerum voluptas saepe nobis consequatur ipsum modi corrupti
+      voluptatem et! Reiciendis repellendus odio nihil, laboriosam maiores
+      minima. Omnis, reiciendis numquam ipsa ut assumenda, ratione deleniti
+      alias ullam quasi obcaecati dicta neque libero placeat quae nobis nisi.
+      Deserunt aspernatur quas earum nihil mollitia quam itaque a sed magni
+      voluptate illo rerum beatae nam, odio exercitationem excepturi, similique
+      laboriosam ab unde obcaecati? Sunt quis repudiandae laudantium natus sint
+      id quod, similique nostrum! Perferendis sunt porro dolore totam? Nemo
+      commodi quod eos temporibus harum enim molestiae atque aliquam dolorum
+      nobis nostrum amet at ducimus, quibusdam nam porro non architecto, maiores
+      assumenda voluptate similique, illum odio minima alias. Vitae ullam ut
+      sequi mollitia pariatur repellendus incidunt. Fugit cupiditate
+      reprehenderit, aspernatur delectus rem cum a. Maiores odit error vero illo
+      voluptates, ipsum earum ut dolorum sequi harum, laudantium alias suscipit
+      distinctio dolor consequatur dolore quos ad incidunt eos esse est amet
+      reprehenderit quo asperiores. Ad distinctio modi quis eveniet facilis
+      suscipit quaerat explicabo, nam non illum aperiam earum voluptatem
+      molestiae a ipsum quas placeat nostrum architecto, provident aspernatur
+      exercitationem ipsam? Reprehenderit laudantium dolorum architecto unde
+      necessitatibus repudiandae tempore iure quos quidem hic! Minima rerum nam
+      quod numquam atque sapiente dolor explicabo similique. Nobis quia delectus
+      ipsam qui magnam? Earum veniam dignissimos corrupti velit nihil at cum?
+      Rem mollitia laudantium impedit dolor praesentium perferendis culpa vero
+      facilis architecto dignissimos placeat, doloremque officia quod aliquam
+      assumenda recusandae ad similique aliquid. Aliquam, velit sapiente. Facere
+      et recusandae corrupti maiores voluptatem tempore nisi, aliquid, ullam
+      illum mollitia dignissimos ipsa at harum ratione in earum hic vel quo.
+      Voluptate fuga delectus quae officiis provident iusto voluptatibus earum
+      temporibus error quos, excepturi, quasi ad. Deserunt nemo id libero
+      dolorum! Repellendus itaque odit accusamus aperiam vel culpa, placeat
+      exercitationem nam, a omnis quibusdam tempore? Inventore, ut commodi sequi
+      recusandae voluptate veritatis beatae nobis soluta non blanditiis
+      reiciendis tenetur deserunt quia pariatur earum error explicabo nemo ad
+      suscipit facere ex nostrum natus. Sit explicabo quasi ducimus.
+    </p>
+    <div>
+      <NuxtLink to="/" id="d0">Index</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style>
+#d0 {
+  margin-right: 40px;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+</style>
